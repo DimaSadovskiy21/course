15d17aa314e35b9cfae7b0ca81c131cb3ec915f1
DimaSadovskiy217 <dmitriy.it@piar.ee>
2024-10-29 22:41:24 +0300

    refactor: rewrite sidebar

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,14 +1,17 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
+
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
+
 import Sidebar from "./Sidebar";
 
 describe("Sidebar", () => {
   test("Test render", () => {
-    render(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
 
   test("toggle Button", () => {
-    render(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId("toggleBtn");
     fireEvent.click(toggleBtn);
     expect(screen.getByTestId("sidebar")).toHaveClass("collapsed");
